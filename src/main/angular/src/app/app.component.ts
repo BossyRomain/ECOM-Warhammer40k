7a3d398a714ffde57501c4b7d51de6b3f444c577
Bylack26 <fabilego3004@gmail.com>
2024-11-07 16:12:55 +0100

    product-sheet

--- src/main/angular/src/app/app.component.ts
+++ src/main/angular/src/app/app.component.ts
@@ -5,7 +5,7 @@ import {Observable} from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular';
