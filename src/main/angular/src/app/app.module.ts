234de2ea08a6d613c4cdbcea87eb003306e3db7f
m2gi-combegae <gaetan.combe@etu.univ-grenoble-alpes.fr>
2024-11-07 15:00:44 +0100

    catalog element et header

--- src/main/angular/src/app/app.module.ts
+++ src/main/angular/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {provideHttpClient} from '@angular/common/http';
+import { CatalogItemComponent } from './catalog-item/catalog-item.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
