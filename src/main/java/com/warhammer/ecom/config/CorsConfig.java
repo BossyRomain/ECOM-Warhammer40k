faa9cb8d2144f2ab5184c84ae18002cbbdadb7f5
BossyRomain <romain.bossy.01@gmail.com>
2024-11-21 16:07:55 +0100

    BACKEND: correction d'une erreur avec les accès aux resources

--- src/main/java/com/warhammer/ecom/config/CorsConfig.java
+++ src/main/java/com/warhammer/ecom/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.warhammer.ecom.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +8,30 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Value("${cors.allowed-methods}")
+    private String[] allowedMethods;
+
+    @Value("${cors.allowed-headers}")
+    private String[] allowedHeaders;
+
+    @Value("${cors.exposed-headers}")
+    private String[] exposedHeaders;
+
+    @Value("${cors.allow-credentials}")
+    private boolean allowCredentials;
+
+    @Value("${cors.max-age}")
+    private long maxAge;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply CORS to all endpoints
-            .allowedOrigins("http://localhost:4200") // Angular app's origin
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // HTTP methods
-            .allowedHeaders("*") // Allow all headers
-            .allowCredentials(true);
+            .allowedOrigins(allowedOrigins) // Angular app's origin
+            .allowedMethods(allowedMethods) // HTTP methods
+            .allowedHeaders(allowedHeaders) // Allow all headers
+            .allowCredentials(allowCredentials);
     }
 }
