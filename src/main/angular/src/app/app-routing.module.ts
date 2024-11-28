2da7beb3a4bd570e2d5b8f041cebcc4bbcc78895
Combegae <gc26@outlook.fr>
2024-11-28 15:01:37 +0100

    Connection client

--- src/main/angular/src/app/app-routing.module.ts
+++ src/main/angular/src/app/app-routing.module.ts
@@ -3,16 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductSheetComponent } from './component/product-sheet/product-sheet.component';
 import { CatalogComponent } from './component/catalog/catalog.component';
 import { connect } from 'http2';
-import { ConnectionComponent } from './connection/connection.component';
+import { ConnectionComponent } from './component/connection/connection.component';
 import { CartComponent } from './component/cart/cart.component';
 
 const routes: Routes = [
   { path: "product/:id", component: ProductSheetComponent },
-  { path: "catalog/:id", component: CatalogComponent },
   { path: "account", component: ConnectionComponent },
   { path: "cart/:id", component: CartComponent },
   { path: "catalog/search", component: CatalogComponent},
-  { path: '**', redirectTo: "catalog"}
+  { path: '**', redirectTo: "catalog/search"}
 ];
 
 @NgModule({
