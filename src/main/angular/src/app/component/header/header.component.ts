9cc7e88bd1185dc976859a50cff9e1de0e923b6f
BossyRomain <romain.bossy.01@gmail.com>
2024-12-11 10:30:49 +0100

    ajout du menu des filtres (choix de quel type de produits filtrer)

--- src/main/angular/src/app/component/header/header.component.ts
+++ src/main/angular/src/app/component/header/header.component.ts
@@ -1,50 +1,72 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { CatalogComponent } from '../catalog/catalog.component';
-import { CartServiceService } from '../../service/cart-service.service';
-import { ClientServiceService } from '../../service/client-service.service';
+import {CommonModule} from '@angular/common';
+import {Component} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CartServiceService} from '../../service/cart-service.service';
+import {ClientServiceService} from '../../service/client-service.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
-  standalone:true,
-  imports:[CommonModule]
+  standalone: true,
+  imports: [CommonModule]
 })
-  
-  
+
+
 export class HeaderComponent {
   cartItemCount: number = 0;
   isconnected: boolean = false;
-  
+
   ngOnInit(): void {
 
     this.cartService.cartItems$.subscribe(count => { //Abonnement a l'observable pour mettre a jour automatiquement le nombre d'article dans le panier
       this.cartItemCount = count;
     });
 
-    this.clientService.isconnected$.subscribe(value =>{
+    this.clientService.isconnected$.subscribe(value => {
       this.isconnected = value;
-      })
+    })
 
 
   }
-  
-  constructor(private router: Router, private activatedRoute:ActivatedRoute, private cartService : CartServiceService, private clientService:ClientServiceService) { }
-  
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private cartService: CartServiceService, private clientService: ClientServiceService) {
+  }
+
   public access_account() {
-    if(this.clientService.isConnected()){
+    if (this.clientService.isConnected()) {
       //this.router.navigate(["/account"]);
       this.router.navigate(["/history", this.clientService.client?.id]);
-    }else{
+    } else {
       this.router.navigate(["/account"]);
     }
-    
+
   }
 
   public filter() {
     //Open Filter menu
+    const elem = document.querySelector('.filters');
+    (elem as HTMLElement).style.display = 'flex';
+  }
+
+  public closeFilters(): void {
+    const elem = document.querySelector('.filters');
+    (elem as HTMLElement).style.display = 'none';
+  }
+
+  public filterSelected(num: number): void {
+    this.closeFilters();
+    switch (num) {
+      case 0:
+        this.router.navigate(["/filter/figurines"]);
+        break
+      case 1:
+        this.router.navigate(["/filter/figurines"]);
+        break
+      case 2:
+        this.router.navigate(["/filter/figurines"]);
+        break
+    }
   }
 
   public help() {
@@ -56,24 +78,28 @@ export class HeaderComponent {
   }
 
   public accesToCart() {
-    if(this.clientService.isConnected()){
+    if (this.clientService.isConnected()) {
       this.router.navigate(['/cart', this.clientService.client?.id])
-    }else{
+    } else {
       this.router.navigate(['/cart', 0]);
     }
-    
+
   }
+
   public search(searchText: string) {
-    this.router.navigate(["/catalog/search"], {relativeTo: this.activatedRoute, queryParams: {search:searchText, page:0}, });
-    
+    this.router.navigate(["/catalog/search"], {
+      relativeTo: this.activatedRoute,
+      queryParams: {search: searchText, page: 0},
+    });
+
   }
 
-  public goToMain(){
-    this.router.navigate(["/catalog/search"], {relativeTo: this.activatedRoute, queryParams: {search:"", page:0}, });
+  public goToMain() {
+    this.router.navigate(["/catalog/search"], {relativeTo: this.activatedRoute, queryParams: {search: "", page: 0},});
   }
 
-  public onEnter(event:KeyboardEvent, searchText:string){
-    if(event.key === "Enter"){
+  public onEnter(event: KeyboardEvent, searchText: string) {
+    if (event.key === "Enter") {
       this.search(searchText);
     }
   }
